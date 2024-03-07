4fa4e77846c3b274fabec26aa1b13d89e7f3f3c3
Andrew Wijaya <33770251+andrewwijaya96@users.noreply.github.com>
2024-03-07 12:13:54 +0700

    Revert "UI Frond end"

--- src/pages/Router.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
-import App from "@/pages/index"; // Update the import to point to the JavaScript file
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  React.createElement(React.StrictMode, null, React.createElement(App, null))
-);
